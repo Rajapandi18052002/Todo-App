cf4d8682d2b6f167cf7fc6c87ab989e1f82b2bb1
Raja pandi <007rajapandi007@gmail.com>
2025-05-06 11:02:22 +0530

    Todo commit

---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import "./App.Css";
+import React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+function App() {
+  let [items, setItems] = useState([
+    { id: 1, label: "Html & CSS" },
+    { id: 2, label: "Javascript" },
+    { id: 3, label: "React Js" },
+  ]);
+  const [newItems, setNewItems] = useState("");
+
+  const [isEdit, setIsEdit] = useState(true);
+
+  let [currentEleID, setCurrentEleID] = useState(null);
+
+  function handleaddsaveitems() {
+    if (isEdit) {
+      setItems([...items, { id: items.length + 1, label: newItems }]);
+      setNewItems("");
+    } else {
+      let editItem = items.map((item) => {
+        return item.id === currentEleID ? { ...item, label: newItems } : item;
+      });
+      setItems(editItem);
+      setNewItems("");
+      setCurrentEleID(null);
+      setIsEdit(true);
+    }
+  }
+
+  function handleUpdate(id) {
+    let finditem = items.find((item) => item.id === id);
+    setNewItems(finditem.label);
+    setIsEdit(false);
+    setCurrentEleID(id);
+  }
+
+  function handledelete(id) {
+    let deleteItem = items
+      .filter((item) => item.id !== id)
+      .map((item, index) => {
+        return { ...item, id: index + 1 };
+      });
+    setItems(deleteItem);
+  }
+
+  return (
+    <>
+      <Box className="bg">
+        <Typography className="typo" variant="h2">
+          {" "}
+          Todo-App{" "}
+        </Typography>
+        <Paper
+          className="paper"
+          sx={{ marginX: "auto" }}
+          square={false}
+          elevation={12}
+        >
+          <Typography align="left" marginLeft="10px">
+            <b>Add Items:</b>
+          </Typography>
+
+          <TextField
+            value={newItems}
+            sx={{ width: "70%", marginLeft: "10px" }}
+            onChange={(e) => {
+              setNewItems(e.target.value);
+            }}
+            id="outlined-basic"
+            label="Add items"
+            variant="outlined"
+          />
+
+          <Button
+            sx={{ marginX: "10px" }}
+            onClick={handleaddsaveitems}
+            variant="contained"
+            size="large"
+            color={isEdit ? "primary" : "success"}
+          >
+            {" "}
+            {isEdit ? "Add" : "edit"}
+          </Button>
+        </Paper>
+
+        <Paper
+          className="paper2"
+          sx={{ marginX: "auto" }}
+          square={false}
+          elevation={14}
+        >
+          {items.map((item) => {
+            return (
+              <Typography
+                sx={{ fontSize: "35px" }}
+                align="left"
+                marginLeft="10px"
+              >
+                {item.id}
+                {"."}
+                {item.label}
+                <FaEdit
+                  id="edit"
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => {
+                    handleUpdate(item.id);
+                  }}
+                />
+
+                <RiDeleteBin5Line
+                  id="delete"
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => {
+                    handledelete(item.id);
+                  }}
+                />
+              </Typography>
+            );
+          })}
+        </Paper>
+      </Box>
+    </>
+  );
+}
+
+export default App;
